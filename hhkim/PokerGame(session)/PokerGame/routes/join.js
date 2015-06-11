75e2e6b0cf98636ff43e679ac674ff8fd2523195
kimhyeonhui <moontia04@naver.com>
2015-06-11 17:44:41 +0900

    게임 기능들 추가

--- hhkim/PokerGame(session)/PokerGame/routes/join.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('join', { title: '회원가입'});
-});
-
-module.exports = router;
